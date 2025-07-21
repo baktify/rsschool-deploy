e245726b5695f436be4ee2e0fef0adf06d7fce5f
Bakyt <bakosyy@gmail.com>
2025-07-21 23:43:51 +0500

    a

--- museum/src/App.jsx
+++ museum/src/App.jsx
@@ -8,7 +8,11 @@ const App = () => (
   <>
     <Helmet>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin="anonymous"
+      />
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Forum&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
--- museum/vite.config.js
+++ museum/vite.config.js
@@ -1,22 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const fixCorsAttribute = () => {
-  return {
-    name: 'fix-cors-attribute',
-    transformIndexHtml(html) {
-      return html.replace(/crossorigin="true"/g, 'crossorigin')
-    },
-  }
-}
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
     }),
-    fixCorsAttribute(),
   ],
   base: '/rsschool-deploy/museum/',
 })
